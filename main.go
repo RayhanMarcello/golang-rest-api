8683d3295975564a95b59cfd959458386f27673d
Rayhan marcello ananda purnomo <rayhanmarcello222@gmail.com>
2025-11-29 00:49:26 +0800

    membuat function handler dan menambahkan methode Post

--- main.go
+++ main.go
@@ -10,14 +10,57 @@ import (
 func main(){
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message" : "testing endpoint root dengan methode GET",
-		})
-	})
+	// get methode
+	router.GET("/", rootHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
 
-	if err := router.Run();
+	// post methode
+	router.POST("/books", postBooksHandler)
+
+	if err := router.Run(":8000");
 	err != nil {
 		log.Fatalf("failed to rn server: %v", err)
+	}
+}
+
+// handler function
+func rootHandler(c *gin.Context){
+	c.JSON(http.StatusOK, gin.H{
+			"message" : "testing endpoint root dengan methode GET",
+		})
+}
+
+func booksHandler(c *gin.Context){
+	id := c.Param("id")
+	title := c.Param("title")
+	c.JSON(http.StatusOK, gin.H{
+		"id" : id,
+		"title" : title,
+	})
+}
+
+func queryHandler(c *gin.Context){
+	query := c.Query("query")
+	c.JSON(http.StatusOK, gin.H{
+		"query" : query,
+	})
+}
+
+type BookInput struct {
+	Title string
+	Price int
+}
+
+func postBooksHandler(c *gin.Context){
+	var bookInput BookInput
+	err := c.ShouldBindJSON(&bookInput)
+	if err != nil {
+		log.Fatalln("error", err)
+	}else{
+		c.JSON(http.StatusOK, gin.H{
+			"Title" : bookInput.Title,
+			"Price" : bookInput.Price,			
+		})
 	}
 }
